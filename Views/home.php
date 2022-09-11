ba1dc8e8744a49b27bd85c17b21e29fd0e66068f
alexandro78 <aleksnkv9@gmail.com>
2022-09-11 11:41:31 +0300

    file limit JavaScript function

--- Views/home.php
+++ Views/home.php
@@ -7,11 +7,23 @@
  <body>
 
  <form enctype="multipart/form-data" action="./../Controllers/MainController.php" method="POST">
-    Send this file: <input name="userfile" type="file" />
+    Send this file: <input id="uploadField" name="userfile" type="file" accept=".csv"/>
     <input type="submit" value="Import" />
 </form>
 <br><br>
 <a href="importList.php">View results</a>
 
+
+
+<script>
+var uploadField = document.getElementById("uploadField");
+
+uploadField.onchange = function() {
+    if(this.files[0].size > 1024){
+       alert("file must not exceed 1MG");
+       this.value = "";
+    };
+};
+</script>
  </body>
 </html>
